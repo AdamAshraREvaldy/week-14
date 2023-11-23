9ba6d9fc63384129253756757a957d8354c935ba
AdamAshraREvaldy <adamashra2@gmail.com>
2023-11-23 09:34:57 +0700

    update UI

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,8 +2,14 @@
 import { useState } from 'react';
 import axios from 'axios';
 
-const AuthForm: React.FC<{ onSubmit: (data: { action: string; [key: string]: string }) => void; buttonText: string }> = ({ onSubmit, buttonText }) => {
-  const [formData, setFormData] = useState({ email: '', password: '' });
+interface AuthFormData {
+  name?: string;
+  email: string;
+  password: string;
+}
+
+const AuthForm: React.FC<{ onSubmit: (data: AuthFormData) => void; buttonText: string }> = ({ onSubmit, buttonText }) => {
+  const [formData, setFormData] = useState<AuthFormData>({ email: '', password: '' });
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
@@ -12,17 +18,19 @@ const AuthForm: React.FC<{ onSubmit: (data: { action: string; [key: string]: str
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     onSubmit(formData);
-  }; 
+  };
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit} className="p-4">
       <label>Email:</label>
-      <input type="email" name="email" value={formData.email} onChange={handleChange} required />
+      <input type="email" name="email" value={formData.email} onChange={handleChange} required className="border border-gray-300 p-2 mb-2" />
 
       <label>Password:</label>
-      <input type="password" name="password" value={formData.password} onChange={handleChange} required />
+      <input type="password" name="password" value={formData.password} onChange={handleChange} required className="border border-gray-300 p-2 mb-2" />
 
-      <button type="submit">{buttonText}</button>
+      <button type="submit" className="bg-blue-500 text-white p-2 rounded">
+        {buttonText}
+      </button>
     </form>
   );
 };
@@ -42,27 +50,29 @@ const BookForm: React.FC<{ onSubmit: (data: { action: string; [key: string]: str
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    onSubmit(formData);
+    onSubmit({ action: 'createBook', ...formData });
   };
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit} className="p-4">
       <label>Title:</label>
-      <input type="text" name="title" value={formData.title} onChange={handleChange} required />
+      <input type="text" name="title" value={formData.title} onChange={handleChange} required className="border border-gray-300 p-2 mb-2" />
 
       <label>Author:</label>
-      <input type="text" name="author" value={formData.author} onChange={handleChange} required />
+      <input type="text" name="author" value={formData.author} onChange={handleChange} required className="border border-gray-300 p-2 mb-2" />
 
       <label>Publisher:</label>
-      <input type="text" name="publisher" value={formData.publisher} onChange={handleChange} required />
+      <input type="text" name="publisher" value={formData.publisher} onChange={handleChange} required className="border border-gray-300 p-2 mb-2" />
 
       <label>Year:</label>
-      <input type="text" name="year" value={formData.year} onChange={handleChange} required />
+      <input type="text" name="year" value={formData.year} onChange={handleChange} required className="border border-gray-300 p-2 mb-2" />
 
       <label>Pages:</label>
-      <input type="text" name="pages" value={formData.pages} onChange={handleChange} required />
+      <input type="text" name="pages" value={formData.pages} onChange={handleChange} required className="border border-gray-300 p-2 mb-2" />
 
-      <button type="submit">{buttonText}</button>
+      <button type="submit" className="bg-blue-500 text-white p-2 rounded">
+        {buttonText}
+      </button>
     </form>
   );
 };
@@ -70,13 +80,23 @@ const BookForm: React.FC<{ onSubmit: (data: { action: string; [key: string]: str
 const HomePage: React.FC = () => {
   const [token, setToken] = useState('');
 
-  const handleLogin = async (data: { email: string; password: string }) => {
+  const handleLogin = async (data: AuthFormData) => {
     try {
       const response = await axios.post('/api', { action: 'login', ...data });
       setToken(response.data.token);
       console.log('User logged in. Token:', response.data.token);
     } catch (error) {
-      console.error('Login failed:', error.response.data.message);
+      console.error('Login failed:', (error as any).response?.data.message);
+    }
+  };
+
+  const handleRegister = async (data: AuthFormData) => {
+    try {
+      const response = await axios.post('/api', { action: 'register', ...data });
+      // Handle registration success (if needed)
+      console.log('User registered. Token:', response.data.token);
+    } catch (error) {
+      console.error('Registration failed:', (error as any).response?.data.message);
     }
   };
 
@@ -84,21 +104,21 @@ const HomePage: React.FC = () => {
     setToken('');
   };
 
-  const handleCreateBook = async (data: { [key: string]: string }) => {
+  const handleCreateBook = async (data: { action: string; [key: string]: string }) => {
     try {
-      const response = await axios.post('/api', { action: 'createBook', ...data });
+      const response = await axios.post('/api', data);
       console.log('Book created:', response.data.book);
     } catch (error) {
-      console.error('Book creation failed:', error.response.data.message);
+      console.error('Book creation failed:', (error as any).response?.data.message);
     }
   };
 
-  const handleUpdateBook = async (data: { [key: string]: string }) => {
+  const handleUpdateBook = async (data: { action: string; [key: string]: string }) => {
     try {
-      const response = await axios.post('/api', { action: 'updateBook', ...data });
+      const response = await axios.post('/api', data);
       console.log('Book updated:', response.data.book);
     } catch (error) {
-      console.error('Book update failed:', error.response.data.message);
+      console.error('Book update failed:', (error as any).response?.data.message);
     }
   };
 
@@ -107,33 +127,40 @@ const HomePage: React.FC = () => {
       const response = await axios.post('/api', { action: 'deleteBook', id: bookId });
       console.log('Book deleted:', response.data.book);
     } catch (error) {
-      console.error('Book deletion failed:', error.response.data.message);
+      console.error('Book deletion failed:', (error as any).response?.data.message);
     }
   };
 
   return (
-    <div>
+    <div className="p-4">
       {!token ? (
         <div>
-          <h1>Login</h1>
+          <h1 className="text-2xl mb-4">Login</h1>
           <AuthForm onSubmit={handleLogin} buttonText="Login" />
+
+          <h1 className="text-2xl mb-4">Register</h1>
+          <AuthForm onSubmit={handleRegister} buttonText="Register" />
         </div>
       ) : (
         <div>
-          <h1>Logout</h1>
-          <button onClick={handleLogout}>Logout</button>
+          <h1 className="text-2xl mb-4">Logout</h1>
+          <button onClick={handleLogout} className="bg-red-500 text-white p-2 rounded">
+            Logout
+          </button>
 
-          <h1>Create Book</h1>
+          <h1 className="text-2xl mb-4">Create Book</h1>
           <BookForm onSubmit={handleCreateBook} buttonText="Create Book" />
 
-          <h1>Update Book</h1>
+          <h1 className="text-2xl mb-4">Update Book</h1>
           <BookForm onSubmit={handleUpdateBook} buttonText="Update Book" />
 
-          <h1>Delete Book</h1>
+          <h1 className="text-2xl mb-4">Delete Book</h1>
           <label>Book ID:</label>
-          <input type="text" onChange={(e) => handleDeleteBook(e.target.value)} />
+          <input type="text" onChange={(e) => handleDeleteBook(e.target.value)} className="border border-gray-300 p-2 mb-2" />
 
-          <button>Delete Book</button>
+          <button onClick={() => handleDeleteBook('')} className="bg-red-500 text-white p-2 rounded">
+            Delete Book
+          </button>
         </div>
       )}
     </div>
